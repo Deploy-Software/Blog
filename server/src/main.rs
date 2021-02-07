fd4556e75c9f12c10e93ad101025e4a6f95a0ad1
Axel Nilsson <theaxelnilsson@gmail.com>
2021-02-07 14:32:10 +0100

    added database connection

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,24 +3,45 @@ use async_graphql::{EmptySubscription, Schema};
 use async_graphql_warp::Response;
 use std::convert::Infallible;
 use warp::{http::Response as HttpResponse, Filter};
-
-struct QueryRoot;
-struct MutationRoot;
+use sqlx::postgres::PgPool;
+use std::env;
 
 mod schema;
 mod records;
 
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+pub struct AuthToken(String);
+struct QueryRoot;
+struct MutationRoot;
+
+pub async fn db_connection() -> Result<PgPool> {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL NOT FOUND");
+    Ok(PgPool::connect(&database_url).await?)
+}
+
 #[tokio::main]
 async fn main() {
+    let pg_pool: PgPool = db_connection().await.expect("Database connection failed.");
+    sqlx::migrate!()
+        .run(&pg_pool)
+        .await
+        .expect("Database migrations failed");
+
     let schema = Schema::build(QueryRoot, MutationRoot, EmptySubscription).finish();
     let graphql_post = warp::path("graphql")
         .and(warp::post())
+        .and(warp::header::optional("token"))
         .and(async_graphql_warp::graphql(schema.clone()))
         .and_then(
-            |(schema, request): (
+            |token,
+             (schema, mut request): (
                 Schema<QueryRoot, MutationRoot, EmptySubscription>,
                 async_graphql::Request,
             )| async move {
+                if let Some(token) = token {
+                    request = request.data(AuthToken(token));
+                }
                 let resp = schema.execute(request).await;
                 Ok::<_, Infallible>(Response::from(resp))
             },
