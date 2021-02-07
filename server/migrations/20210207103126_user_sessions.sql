ef328feaf9d5eee9b6bf418cddfdd0dd1516405e
Axel Nilsson <theaxelnilsson@gmail.com>
2021-02-07 11:33:23 +0100

    added migrations

---
+++ server/migrations/20210207103126_user_sessions.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user_sessions (
+     id serial PRIMARY KEY,
+     user_id integer NOT NULL REFERENCES users (id),
+     token varchar(255) NOT NULL,
+     date timestamp with time zone NOT NULL DEFAULT now()
+);
+CREATE UNIQUE INDEX user_sessions_idx ON user_sessions (token);
